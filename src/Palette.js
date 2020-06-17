b688220016209256c9ff949ee605a4b684a47cbf
sungnga <nga422@gmail.com>
2020-06-17 11:03:20 -0700

    Style color slider, override default styles

--- src/Palette.js
+++ src/Palette.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
+
+// NOTE: ORDER OF IMPORT IS IMPORTANT!
+// To override the default styles, import your style file after the vendor style file
+import 'rc-slider/assets/index.css';
 import './Palette.css';
 import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
 
 export default class Palette extends Component {
 	constructor(props) {
@@ -22,13 +25,15 @@ export default class Palette extends Component {
 		));
 		return (
 			<div className='Palette'>
-				<Slider
-					defaultValue={level}
-					min={100}
-					max={900}
-					step={100}
-					onAfterChange={this.changeLevel}
-				/>
+				<div className='slider'>
+					<Slider
+						defaultValue={level}
+						min={100}
+						max={900}
+						step={100}
+						onAfterChange={this.changeLevel}
+					/>
+				</div>
 				{/* Navbar goes here */}
 				<div className='Palette-colors'>{colorBoxes}</div>
 				{/* footer */}
